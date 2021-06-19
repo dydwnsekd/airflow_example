52788b7846433cf15eb9f1ecc96be05ac46b2249
dydwnsekd <dydwnsekd123@gmail.com>
2021-06-19 20:57:00 +0900

    KakaoWorkHook Block 관련 함수 분리

--- customOperator/KakaoWorkHook.py
+++ customOperator/KakaoWorkHook.py
@@ -30,7 +30,6 @@ class KakaoWorkhookHook(HttpHook):
             "Authorization": "Bearer {app_key}".format(app_key=app_key),
             "Content-Type": "application/json"
         }
-     
  
     def makeKakaoMessage(self, text):
         if not self.block:
@@ -45,48 +44,53 @@ class KakaoWorkhookHook(HttpHook):
                 "blocks": self.block_list
             })
  
-    def kakaoBlocksMaker(
+    def textBlock(
         self,
-        type='text',
         text='text',
-        style='default',
-        image_url='https://airflow.apache.org/docs/apache-airflow/stable/_images/pin_large.png',
-        action_type='open_system_browser',
-        value="localhost:8080"
     ):
-     
-        if type == 'text':
-            self.block_list.append({
+        self.block_list.append({
                 "type": "text",
                 "text": "{text}".format(text=text),
                 "markdown": False
             })
  
-        elif type == 'button':
-            # style : ['default', 'primary', 'danger']
-            self.block_list.append({
+    def buttonBlock(
+        self,
+        text='text',
+        style='default',
+        action_type='open_system_browser',
+        action_name='',
+        value="localhost:8080"
+    ):
+        self.block_list.append({
                 "type": "button",
                 "text": "{text}".format(text=text),
                 "style": "{style}".format(style=style),
                 "action_type": "{action_type}".format(action_type=action_type),
+                "action_name": "{action_name}".format(action_name=action_name),
                 "value": "{value}".format(value=value)
             })
-         
+ 
+    def headerBlock(
+        self,
+        text='text',
+        style='blue'
+    ):
         # header을 사용하는 경우 제일 상위에서만 사용 가능
         # kakaoBlocksMaker에서 제일 먼저 add 필요
-        elif type == 'header':
-            # style : ['blue', 'red', 'yellow']
-            style = 'blue'
-            self.block_list.append({
-                "type": "header",
-                "text": "{text}".format(text=text),
-                "style": "{style}".format(style=style)
-            })
-             
-        elif type == 'image_link':
-            self.block_list.append({
+        self.block_list.append({
+            "type": "header",
+            "text": "{text}".format(text=text),
+            "style": "{style}".format(style=style)
+        })
+     
+    def imagelinkBlock(
+        self,
+        url="https://airflow.apache.org/docs/apache-airflow/stable/_images/pin_large.png"
+    ):
+        self.block_list.append({
                 "type": "image_link",
-                "url": "{url}".format(url=image_url)
+                "url": "{url}".format(url=url)
             })
  
     def sendMessage(self, data) -> None:
